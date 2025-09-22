4ff7f807d6303f475a0e7429d339eede901ab80d
llussy <llussy.me@gmail.com>
2025-09-22 11:53:39 +0800

    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,10 +15,10 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: 'llussy Running',
+  siteUrl: 'https://llussy.github.io/running_page/',
+  logo: 'https://avatars.githubusercontent.com/u/18432966?v=4',
+  description: 'llussy',
   navLinks: [
     {
       name: 'Summary',
